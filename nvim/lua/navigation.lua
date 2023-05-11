c8ae79c5019ea684a5f5fa4d32e174a9dbb133e0
a.belo <a.belo@prom.ua>
2023-05-11 17:34:03 +0300

    fix: typos

--- nvim/lua/navigation.lua
+++ nvim/lua/navigation.lua
@@ -43,10 +43,10 @@ tmap('<A-h>', '<C-\\><C-N><C-w>h')
 tmap('<A-j>', '<C-\\><C-N><C-w>j')
 tmap('<A-k>', '<C-\\><C-N><C-w>k')
 tmap('<A-l>', '<C-\\><C-N><C-w>l')
-tmap('<A-h>', '<C-w>h')
-tmap('<A-j>', '<C-w>j')
-tmap('<A-k>', '<C-w>k')
-tmap('<A-l>', '<C-w>l')
+nmap('<A-h>', '<C-w>h')
+nmap('<A-j>', '<C-w>j')
+nmap('<A-k>', '<C-w>k')
+nmap('<A-l>', '<C-w>l')
 
 -- Move to last used window
 nmap('<A-;>', '<C-w>p')
@@ -121,9 +121,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
